3dd9417d922e250e8f0c1889687ac50ecdca9601
sapli97 <sarapileio@gmail.com>
2024-02-29 14:02:24 +0100

    inserito service Category

---
+++ src/main/java/Services/CategoryService.java
@@ -0,0 +1,73 @@
+package Services;
+
+import com.esercizio.toDoList.dao.CategoriesDao;
+
+import com.esercizio.toDoList.entities.Categories;
+import com.esercizio.toDoList.entities.Entity;
+import com.esercizio.toDoList.entities.Tasks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class CategoryService {
+
+        @Autowired
+        private CategoriesDao categoriesDao;
+        @Autowired
+        private ApplicationContext context;
+
+        public List<Categories> findAll(){
+
+            //salvo i dati della read nella mappa che abbiamo chiamata data
+            Map<Integer, Entity> data = categoriesDao.read();
+            List<Categories> ris = new ArrayList<>();
+
+            //con il ciclo for cicliamo i valori della mappa data e li salviamo nella lista paziente
+            for(Entity e : data.values()){
+                if(e instanceof Tasks){
+                    ris.add((Categories) e);
+                }
+            }
+
+            return ris;
+        }
+
+        public Categories findById(int id){
+            Map<Integer, Entity> data = categoriesDao.read();
+
+            return (Categories) data.get(id);
+        }
+
+        public List<Categories> findByNome(String nome){
+            Map<Integer, Entity> data = categoriesDao.read();
+            List<Categories> ris = new ArrayList<>();
+
+            for(Entity e : data.values()){
+                if(e instanceof Categories){
+                    ris.add((Categories) e);
+                }
+            }
+
+            return ris;
+        }
+
+
+        public void insertCategory(Map<String, String> params){
+            Categories c = context.getBean(Categories.class, params);
+            categoriesDao.create(c);
+        }
+
+        public void updateCategory(Map<String, String> params){
+            Categories c = context.getBean(Categories.class, params);
+            categoriesDao.update(c);
+        }
+
+        public void deleteCategory(int id){
+            categoriesDao.delete(id);
+        }
+    
+
+}
